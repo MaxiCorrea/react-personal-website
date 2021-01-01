80b008fb14f93b3f6576f2213576dd40a099c44e
Maximiliano Correa <jmaxicorrea@gmail.com>
2021-01-01 09:01:59 -0300

    Remove Code for test Page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,8 +1,5 @@
 import * as React from "react";
 import { ContactContainer } from "../../components/contact/ContactContainer/ContactContainer";
-import { ContactField } from "../../components/contact/ContactField/ContactField";
-import { ContactForm } from "../../components/contact/ContactForm/ContactForm";
-import { ContactSend } from "../../components/contact/ContactSend/ContactSend";
 import { ContactTitle } from "../../components/contact/ContactTitle/ContactTitle";
 import { useStyles } from "./Contact.styles";
 
@@ -12,11 +9,6 @@ export const Contact: React.FC = () => {
     <div className={classes.root}>
       <ContactContainer>
         <ContactTitle text={"Contact"} />
-        <ContactForm>
-          <ContactField caption={"Full Name"} inputMode={"text"} />
-          <ContactField caption={"Email Address"} inputMode={"email"} />
-          <ContactSend caption={"Send Message"}/>
-        </ContactForm>
       </ContactContainer>
     </div>
   );
