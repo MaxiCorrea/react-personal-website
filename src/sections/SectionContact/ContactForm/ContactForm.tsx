716ae1a7d700632baf4425fd9581a5bc7bd23209
MaxiCorrea <jmaxicorrea@gmail.com>
2021-08-07 23:04:20 -0300

    Adding local state for ContactFrom Component

--- src/sections/SectionContact/ContactForm/ContactForm.tsx
+++ src/sections/SectionContact/ContactForm/ContactForm.tsx
@@ -5,27 +5,38 @@ import { Button } from "components/button/Button";
 
 export const ContactForm: React.FC = (props) => {
   const classes = useStyles();
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [phone, setPhone] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  
   return (
     <div className={classes.container}>
       <p className={classes.title}>Send me a message</p>
       <TextField
         id="name"
         style={{ width: "90%" }}
-        onClear={() => {}}
+        onClear={() => {setName("")}}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>)=> {setName(e.target.value);}}
+        value={name}
         label="Name"
       />
       <div style={{ height: "15px" }}></div>
       <TextField
         id="email"
         style={{ width: "90%" }}
-        onClear={() => {}}
+        value={email}
+        onClear={() => {setEmail("")}}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>)=> {setEmail(e.target.value);}}
         label="Email"
       />
       <div style={{ height: "15px" }}></div>
       <TextField
         id="phone-number"
         style={{ width: "90%" }}
-        onClear={() => {}}
+        value={phone}
+        onClear={() => {setPhone("")}}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>)=> {setPhone(e.target.value);}}
         label="Phone Number"
       />
       <div style={{ height: "15px" }}></div>
@@ -34,7 +45,9 @@ export const ContactForm: React.FC = (props) => {
         rows={5}
         multiline
         style={{ width: "90%" }}
-        onClear={() => {}}
+        value={message}
+        onClear={() => {setMessage("")}}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>)=> {setMessage(e.target.value);}}
         label="Message"
       />
       <div style={{ height: "15px" }}></div>
